3d90a9d2e64a26c9042dc3c03d3751158ed88f95
Luke Brannon <luke.brannon@gmail.com>
2019-06-11 05:00:42 +0000

    change initial email bool to display

--- src/docent/hoa/houses/content/hoa_annual_inspection.py
+++ src/docent/hoa/houses/content/hoa_annual_inspection.py
@@ -93,7 +93,8 @@ class IHOAAnnualInspection(form.Schema):
         description=_(u""),
     )
 
-    form.mode(initial_email_sent='hidden')
+#    form.mode(initial_email_sent='hidden')
+    form.mode(initial_email_sent='display')
     initial_email_sent = schema.Bool(
         title = _(u'Initial Email Sent'),
         description = _(u''),
